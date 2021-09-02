3049b6eb0aedd655bc7affc613d6d4ecf12d5f61
matheusgvieira <matheusmaxi9.0@gmail.com>
2021-09-02 01:30:13 -0300

    :construction: Voice control recog

---
+++ voice/README.md
@@ -0,0 +1,13 @@
+# Voide Control
+
+## Install librarys
+```sh
+pip install SpeechRecognition
+pip install PyAudio
+```
+
+## Run
+
+```sh
+python voice.py
+```
---
+++ voice/voice.py
@@ -0,0 +1,41 @@
+# Example without Google Assistant
+# video: https://youtu.be/R1SFP3t7Gwo
+
+import speech_recognition as sr
+from datetime import date
+from time import sleep
+import os
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+print("########## DEBUG ###################")
+
+while True:
+    with mic as source:
+        audio = r.listen(source)
+    if audio:
+        words = r.recognize_google(audio)
+        print(words)
+
+    if words == "blue":
+        os.system('../led/./rgb.out blue')
+    
+    if words == "red":
+        os.system('../led/./rgb.out red')
+
+    if words == "green":
+        os.system('../led/./rgb.out green')
+
+    if words == "off":
+        os.system('../led/./rgb.out off')
+
+    if words == "exit":
+        print("...")
+        sleep(1)
+        print("...")
+        sleep(1)
+        print("...")
+        sleep(1)
+        print("Goodbye")
+        break
